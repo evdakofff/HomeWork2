561ad50936155cb288d5c13457cf3b6e0fc34efc
evdakofff <evdakofff@ya.ru>
2024-08-04 20:34:18 +0300

    Add files via upload

--- module_1_3.py
+++ module_1_3.py
@@ -2,8 +2,8 @@ name = 'Dmitry'
 print('Name:' + name)
 age = 32
 print('Age:' + str(age))
-new_age = age + 1
-print('New Age:' + str(new_age))
+age = age + 1
+print('New Age:' + str(age))
 is_student = True
 print('Is Student:' + str(is_student))
 
